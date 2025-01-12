5bc6bd5a7f0f52ea7f0a5d127653e0e87f963366
okok801 <okok801@gmail.com>
2025-01-12 22:06:44 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -1,62 +1,62 @@
 const CONFIG = {
-  HEO_HOME_POST_TWO_COLS: true, // 首页博客两列显示，若为false则只显示一列
-  HEO_LOADING_COVER: true, // 页面加载的遮罩动画
+  HEO_HOME_POST_TWO_COLS: true, // 首頁博客兩列顯示，若爲false則只顯示一列
+  HEO_LOADING_COVER: true, // 頁面加載的遮罩動畫
 
   HEO_HOME_BANNER_ENABLE: true,
 
-  HEO_SITE_CREATE_TIME: '2025-01-25', // 建站日期，用于计算网站运行的第几天
+  HEO_SITE_CREATE_TIME: '2025-01-25', // 建站日期，用于計算網站運行的第幾天
 
-  // 首页顶部通知条滚动内容，如不需要可以留空 []
+  // 首頁頂部通知條滾動內容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
-   // { title: '欢迎来到我的博客', url: 'https://blog.tangly1024.com' },
-   // { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
+   // { title: '歡迎來到我的博客', url: 'https://blog.tangly1024.com' },
+   // { title: '訪問文檔中心獲取更多幫助', url: 'https://docs.tangly1024.com' }
   ],
 
-  // 英雄区左右侧组件颠倒位置
+  // 英雄區左右側組件顛倒位置
   HEO_HERO_REVERSE: false,
-  // 博客主体区左右侧组件颠倒位置
+  // 博客主體區左右側組件顛倒位置
   HEO_HERO_BODY_REVERSE: false,
 
-  // 英雄区(首页顶部大卡)
-   //HEO_HERO_TITLE_1: '分享编程',
-   //HEO_HERO_TITLE_2: '与思维认知',
+  // 英雄區(首頁頂部大卡)
+   //HEO_HERO_TITLE_1: '分享編程',
+   //HEO_HERO_TITLE_2: '與思維認知',
   // HEO_HERO_TITLE_3: 'TANGLY1024.COM',
-  // HEO_HERO_TITLE_4: '新版上线',
-   //HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
+  // HEO_HERO_TITLE_4: '新版上綫',
+   //HEO_HERO_TITLE_5: 'NotionNext4.0 輕鬆定制主題',
    //HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
-  // 英雄区遮罩文字
-  // HEO_HERO_COVER_TITLE: '随便逛逛',
+  // 英雄區遮罩文字
+  // HEO_HERO_COVER_TITLE: '隨便逛逛',
 
-  // 英雄区显示三个置顶分类
-  HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
-  HEO_HERO_CATEGORY_2: { title: '热门文章', url: '/tag/热门文章' },
-  HEO_HERO_CATEGORY_3: { title: '实用教程', url: '/tag/实用教程' },
+  // 英雄區顯示三個置頂分類
+  HEO_HERO_CATEGORY_1: { title: '必看精選', url: '/tag/必看精選' },
+  HEO_HERO_CATEGORY_2: { title: '熱門文章', url: '/tag/熱門文章' },
+  HEO_HERO_CATEGORY_3: { title: '實用教程', url: '/tag/實用教程' },
 
-  // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
-  HEO_HERO_RECOMMEND_POST_TAG: '推荐',
-  HEO_HERO_RECOMMEND_POST_SORT_BY_UPDATE_TIME: false, // 推荐文章排序，为`true`时将强制按最后修改时间倒序
-  //   HERO_RECOMMEND_COVER: 'https://cdn.pixabay.com/photo/2015/10/30/20/13/sunrise-1014712_1280.jpg', // 英雄区右侧图片
+  // 英雄區右側推薦文章標簽, 例如 [推薦] , 最多六篇文章; 若留空白''，則推薦最近更新文章
+  HEO_HERO_RECOMMEND_POST_TAG: '推薦',
+  HEO_HERO_RECOMMEND_POST_SORT_BY_UPDATE_TIME: false, // 推薦文章排序，爲`true`時將强制按最後修改時間倒序
+  //   HERO_RECOMMEND_COVER: 'https://cdn.pixabay.com/photo/2015/10/30/20/13/sunrise-1014712_1280.jpg', // 英雄區右側圖片
 
-  // 右侧个人资料卡牌欢迎语，点击可自动切换
+  // 右側個人資料卡牌歡迎語，點擊可自動切換
   HEO_INFOCARD_GREETINGS: [
     '你好！我是',
-    '🔍 分享与热心帮助',
-    '🤝 专修交互与设计',
-    '🏃 脚踏实地行动派',
+    '🔍 分享與熱心幫助',
+    '🤝 專修交互與設計',
+    '🏃 脚踏實地行動派',
     '🏠 智能家居小能手',
-    '🤖️ 数码科技爱好者',
-    '🧱 团队小组发动机'
+    '🤖️ 數碼科技愛好者',
+    '🧱 團隊小組發動機'
   ],
 
-  // 个人资料底部按钮
+  // 個人資料底部按鈕
   // HEO_INFO_CARD_URL1: '/about',
  //  HEO_INFO_CARD_ICON1: 'fas fa-user',
  //  HEO_INFO_CARD_URL2: 'https://github.com/tangly1024',
  //  HEO_INFO_CARD_ICON2: 'fab fa-github',
  //  HEO_INFO_CARD_URL3: 'https://www.tangly1024.com',
- //  HEO_INFO_CARD_TEXT3: '了解更多',
+ //  HEO_INFO_CARD_TEXT3: '瞭解更多',
 
-  // 用户技能图标
+  // 用戶技能圖標
   HEO_GROUP_ICONS: [
     {
       title_1: 'AfterEffect',
@@ -116,43 +116,43 @@ const CONFIG = {
     }
   ],
 
-  HEO_SOCIAL_CARD: true, // 是否显示右侧，点击加入社群按钮
-  HEO_SOCIAL_CARD_TITLE_1: '交流频道',
-  HEO_SOCIAL_CARD_TITLE_2: '加入我们的社群讨论分享',
-  HEO_SOCIAL_CARD_TITLE_3: '点击加入社群',
+  HEO_SOCIAL_CARD: true, // 是否顯示右側，點擊加入社群按鈕
+  HEO_SOCIAL_CARD_TITLE_1: '交流頻道',
+  HEO_SOCIAL_CARD_TITLE_2: '加入我們的社群討論分享',
+  HEO_SOCIAL_CARD_TITLE_3: '點擊加入社群',
   HEO_SOCIAL_CARD_URL: 'https://docs.tangly1024.com/article/how-to-question',
 
-  // 底部统计面板文案
-  HEO_POST_COUNT_TITLE: '文章数:',
-  HEO_SITE_TIME_TITLE: '建站天数:',
-  HEO_SITE_VISIT_TITLE: '访问量:',
-  HEO_SITE_VISITOR_TITLE: '访客数:',
+  // 底部統計面板文案
+  HEO_POST_COUNT_TITLE: '文章數:',
+  HEO_SITE_TIME_TITLE: '建站天數:',
+  HEO_SITE_VISIT_TITLE: '訪問量:',
+  HEO_SITE_VISITOR_TITLE: '訪客數:',
 
-  // *****  以下配置无效，只是预留开发 ****
-  // 菜单配置
-  HEO_MENU_INDEX: true, // 显示首页
-  HEO_MENU_CATEGORY: true, // 显示分类
-  HEO_MENU_TAG: true, // 显示标签
-  HEO_MENU_ARCHIVE: true, // 显示归档
-  HEO_MENU_SEARCH: true, // 显示搜索
+  // *****  以下配置無效，只是預留開發 ****
+  // 菜單配置
+  HEO_MENU_INDEX: true, // 顯示首頁
+  HEO_MENU_CATEGORY: true, // 顯示分類
+  HEO_MENU_TAG: true, // 顯示標簽
+  HEO_MENU_ARCHIVE: true, // 顯示歸檔
+  HEO_MENU_SEARCH: true, // 顯示搜索
 
-  HEO_POST_LIST_COVER: true, // 列表显示文章封面
-  HEO_POST_LIST_COVER_HOVER_ENLARGE: false, // 列表鼠标悬停放大
+  HEO_POST_LIST_COVER: true, // 列表顯示文章封面
+  HEO_POST_LIST_COVER_HOVER_ENLARGE: false, // 列表鼠標懸停放大
 
-  HEO_POST_LIST_COVER_DEFAULT: true, // 封面为空时用站点背景做默认封面
+  HEO_POST_LIST_COVER_DEFAULT: true, // 封面爲空時用站點背景做默認封面
   HEO_POST_LIST_SUMMARY: true, // 文章摘要
-  HEO_POST_LIST_PREVIEW: false, // 读取文章预览
-  HEO_POST_LIST_IMG_CROSSOVER: true, // 博客列表图片左右交错
+  HEO_POST_LIST_PREVIEW: false, // 讀取文章預覽
+  HEO_POST_LIST_IMG_CROSSOVER: true, // 博客列表圖片左右交錯
 
-  HEO_ARTICLE_ADJACENT: true, // 显示上一篇下一篇文章推荐
-  HEO_ARTICLE_COPYRIGHT: true, // 显示文章版权声明
-  HEO_ARTICLE_RECOMMEND: true, // 文章关联推荐
+  HEO_ARTICLE_ADJACENT: true, // 顯示上一篇下一篇文章推薦
+  HEO_ARTICLE_COPYRIGHT: true, // 顯示文章版權聲明
+  HEO_ARTICLE_RECOMMEND: true, // 文章關聯推薦
 
-  HEO_WIDGET_LATEST_POSTS: true, // 显示最新文章卡
-  HEO_WIDGET_ANALYTICS: false, // 显示统计卡
+  HEO_WIDGET_LATEST_POSTS: true, // 顯示最新文章卡
+  HEO_WIDGET_ANALYTICS: false, // 顯示統計卡
   HEO_WIDGET_TO_TOP: true,
-  HEO_WIDGET_TO_COMMENT: true, // 跳到评论区
-  HEO_WIDGET_DARK_MODE: true, // 夜间模式
-  HEO_WIDGET_TOC: true // 移动端悬浮目录
+  HEO_WIDGET_TO_COMMENT: true, // 跳到評論區
+  HEO_WIDGET_DARK_MODE: true, // 夜間模式
+  HEO_WIDGET_TOC: true // 移動端懸浮目錄
 }
 export default CONFIG
